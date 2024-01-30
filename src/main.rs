905bd27ae4cf546b226853400daa77ba3b69c248
frshd <frshd@frshds-MacBook-Pro.local>
2024-01-30 12:08:09 -0500

    Read audio file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::{fs::File, io::Write};
-
+use hound;
 fn show_info() {
     eprintln!("MUSI-6106 Assignment Executable");
     eprintln!("(c) 2024 Stephen Garrett & Ian Clester");
+}
+
+fn convert_sample(sample: i16) -> f32 {
+    sample as f32 / i16::MAX as f32
 }
 
 fn main() {
@@ -11,12 +15,50 @@ fn main() {
     // Parse command line arguments
     // First argument is input .wav file, second argument is output text file.
     let args: Vec<String> = std::env::args().collect();
-    // TODO: your code here
+    let mut reader = match hound::WavReader::open(&args[1]) {
+        Ok(reader) => reader,
+        Err(error) => {
+            eprintln!("Error opening file: {:?}", error);
+            return;
+        }
+    };
 
-    // Open the input wave file and determine number of channels
-    // TODO: your code here; see `hound::WavReader::open`.
+    let spec = reader.spec();
+    dbg!(spec.bits_per_sample);
+    dbg!(spec.channels);
+    dbg!(spec.sample_format);
+    dbg!(spec.sample_rate);
+    let num_channels = spec.channels as usize;
 
-    // Read audio data and write it to the output text file (one column per channel)
-    // TODO: your code here; we suggest using `hound::WavReader::samples`, `File::create`, and `write!`.
-    //       Remember to convert the samples to floating point values and respect the number of channels!
+    let mut output_file = match File::create(&args[2]) {
+        Ok(file) => file,
+        Err(error) => {
+            eprintln!("Error creating output file: {:?}", error);
+            return;
+        }
+    };
+    let mut frame_samples = Vec::with_capacity(num_channels);
+    for (i, iterated_sample) in reader.samples::<i16>().enumerate() {
+        match iterated_sample {
+            Ok(sample) => {
+                frame_samples.push(convert_sample(sample));
+
+                if frame_samples.len() == num_channels {
+                    if let Err(e) = writeln!(
+                        output_file, "{}",
+                        frame_samples.iter().map(|s| s.to_string()).collect::<Vec<_>>().join(" ")
+                    )
+                    {
+                        eprintln!("Error writing to output file: {:?}", e);
+                        break;
+                    }
+                    frame_samples.clear();
+                }
+            },
+            Err(error) => {
+                eprintln!("Error reading sample: {:?}", error);
+                break;
+            }
+        }
+    }
 }
